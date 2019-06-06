a41a08ebc3aec9cec2e1c3201257597b58b7ce08
chengcaiyun <448237504@qq.com>
2019-06-06 19:07:19 +0800

    第一次上传主分支

--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ import "./stylesheets/main.scss"
 //引入rem。js文件
 import "./modules/rem"
 //引入swiper.min.css文件
-<<<<<<< HEAD
 import 'swiper/dist/css/swiper.min.css'
 //引入axios
 import axios from "axios"
@@ -25,12 +24,6 @@ Vue.component("mt-button", Button);
 Vue.component("mt-tabbar", Tabbar);
 Vue.component("mt-tab-item", TabItem);
 Vue.component("mt-cell", Cell);
-=======
-import "swiper/dist/css/swiper.min.css"
-//引进来之后绑定在vue的prototype上
-import axios from "axios"
-Vue.prototype.$http = axios;
->>>>>>> 887bf868cf30d149d7649e8d87422961abd9614e
 new Vue({
   router,
   store,
--- src/router/index.js
+++ src/router/index.js
@@ -7,22 +7,13 @@ import home from "./home"
 import found from "./found"
 import mine from "./mine"
 import ticket from "./ticket"
-<<<<<<< HEAD
 import MovieDetail from "./moviedetail"
-export default new Router({
-  routes: [
-    {path:"/",redirect:"/home"},
-    home,found,mine,ticket,MovieDetail,
-    {path:"/notfound",component:()=>import("@/views/Notfound")},
-    {path:"*",redirect:"/notfound"}
-=======
 import actor from "./actor"
 export default new Router({
   routes: [
     {path:"/",redirect:"/home"},
-    home,found,mine,ticket,actor
-    // {path:"/notfound",component:()=>import("@/views/Notfound")},
-    // {path:"*",redirect:"/notfound"}
->>>>>>> 887bf868cf30d149d7649e8d87422961abd9614e
+    home,found,mine,ticket,MovieDetail,actor,
+    {path:"/notfound",component:()=>import("@/views/Notfound")},
+    {path:"*",redirect:"/notfound"}
   ]
 })
